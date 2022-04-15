7c63287659f31092cc8002875f17072f821f306a
ANAS EL ABEDELALAOUI <72227922+AnasElAbedelalaoui@users.noreply.github.com>
2022-04-15 04:23:39 +0200

    Add files via upload

---
+++ src/tests/DateTimerTest.java
@@ -0,0 +1,57 @@
+package timer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+import java.util.Vector;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class DateTimerTest {
+	static private Vector<Integer> vec1;
+	static private DateTimer datetime1;
+	static private Iterator<Integer> it;
+	static private TreeSet<Integer> ts;
+	static private DateTimer datetime2;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		vec1 = new Vector<>();
+    	vec1.add(7);
+    	vec1.add(8);
+    	it = vec1.iterator();
+    	datetime1 = new DateTimer(vec1);
+    	ts = new TreeSet<>();
+    	ts.add(1);
+    	ts.add(2);
+    	ts.add(3);
+    	datetime2 = new DateTimer(ts);
+	}
+
+
+	@Test
+	void ts1() {
+		int t = datetime2.it.next();
+		Assert.assertEquals(1,t);
+	}
+
+	@Test
+	void ts2() {
+		Assert.assertEquals(vec1, this.datetime1.lapsTimes);
+	}
+
+	@Test
+	void ts3() {
+		Assert.assertEquals(true, DateTimerTest.datetime1.hasNext());
+	}
+
+	@Test
+	void ts4() {
+		int value = DateTimerTest.datetime1.next();
+		Assert.assertEquals(7, value);
+	}
+
+}
---
+++ src/tests/OneShotTimerTest.java
@@ -0,0 +1,47 @@
+package timer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import junit.framework.Assert;
+import java.lang.reflect.Field;
+import java.util.Iterator;
+
+class OneShotTimerTest {
+    private static Integer at1;
+    private static boolean hasNext;
+    private static OneShotTimer oneShotTimer;
+    private static Field[] reflexionFields;
+	@BeforeAll
+	static void setUpBeforeClass() {
+		at1 = 1;
+		oneShotTimer = new OneShotTimer(at1);
+		Class<?> reflexionOneShotTimer = oneShotTimer.getClass();
+		reflexionFields = reflexionOneShotTimer.getDeclaredFields();
+
+		for (Field field: reflexionFields) { 
+			field.setAccessible(true);
+		}
+	}
+
+	@Test
+	void ts5() throws IllegalArgumentException, IllegalAccessException {
+		Assert.assertEquals(true, oneShotTimer.hasNext());
+		Assert.assertEquals(at1, reflexionFields[0].get(oneShotTimer));
+	}
+	
+	@Test
+	void ts6() throws IllegalArgumentException, IllegalAccessException {
+		Assert.assertEquals(true, oneShotTimer.hasNext());
+	}
+	
+	@Test
+	void ts7() throws IllegalArgumentException, IllegalAccessException {
+		Integer d = oneShotTimer.next();
+		Assert.assertEquals(at1, d);
+		Assert.assertEquals(false, oneShotTimer.hasNext());
+		Assert.assertEquals(null, reflexionFields[0].get(oneShotTimer));
+	}
+}

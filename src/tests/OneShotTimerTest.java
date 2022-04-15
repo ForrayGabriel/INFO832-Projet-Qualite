3b07032596aa466b2cde2dc09518b2972a2fd6e9
ANAS EL ABEDELALAOUI <72227922+AnasElAbedelalaoui@users.noreply.github.com>
2022-04-15 11:29:45 +0200

    Update OneShotTimerTest.java

--- src/tests/OneShotTimerTest.java
+++ src/tests/OneShotTimerTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import junit.framework.Assert;
-import timer.OneShotTimer;
-
 import java.lang.reflect.Field;
 import java.util.Iterator;
 
@@ -23,26 +21,27 @@ class OneShotTimerTest {
 		Class<?> reflexionOneShotTimer = oneShotTimer.getClass();
 		reflexionFields = reflexionOneShotTimer.getDeclaredFields();
 
-		for (Field field: reflexionFields) {
+		for (Field field: reflexionFields) { 
 			field.setAccessible(true);
 		}
 	}
 
 	@Test
-	void testOneShotTimerforint() throws IllegalArgumentException, IllegalAccessException {
-		
-		assertTrue(oneShotTimer.hasNext());
-		assertEquals(at1, reflexionFields[0].get(oneShotTimer));
+	void ts5() throws IllegalArgumentException, IllegalAccessException {
+		Assert.assertEquals(true, oneShotTimer.hasNext());
+		Assert.assertEquals(at1, reflexionFields[0].get(oneShotTimer));
 	}
+	
 	@Test
-	void testhasNext() throws IllegalArgumentException, IllegalAccessException {
-		assertTrue(oneShotTimer.hasNext());
+	void ts6() throws IllegalArgumentException, IllegalAccessException {
+		Assert.assertEquals(true, oneShotTimer.hasNext());
 	}
+	
 	@Test
-	void testnext() throws IllegalArgumentException, IllegalAccessException {
+	void ts7() throws IllegalArgumentException, IllegalAccessException {
 		Integer d = oneShotTimer.next();
-		assertEquals(at1, d);
-		assertFalse(oneShotTimer.hasNext());
-		assertNull(reflexionFields[0].get(oneShotTimer));
+		Assert.assertEquals(at1, d);
+		Assert.assertEquals(false, oneShotTimer.hasNext());
+		Assert.assertEquals(null, reflexionFields[0].get(oneShotTimer));
 	}
 }
